c73f1fe57409684d3a9e40eee3eeaece07cd2916
thinkAmi <dev.thinkami@gmail.com>
2022-06-29 08:26:01 +0900

    状態ホスティングを追加

--- app/src/main/java/com/example/basiccodelab/MainActivity.kt
+++ app/src/main/java/com/example/basiccodelab/MainActivity.kt
@@ -4,13 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedButton
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -28,7 +24,64 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
+fun MyApp() {
+    // 様々な画面を表示するロジックを追加し、状態をホイストする
+
+    // by は毎回 .value を入力する手間を省くためのプロパティデリゲート
+    var shouldShowOnbording by remember { mutableStateOf(true) }
+
+    if (shouldShowOnbording) {
+        // イベントが発生したときに通知するためのコールバックを渡す
+        // 状態ではなく関数を渡すことで、コンポーサブルを再利用しやすく、
+        // かつ、他のコンポーザブルによって変更されないよう、状態を保護する
+        OnboardingScreen(onContinueClicked = { shouldShowOnbording = false })
+    } else {
+        Greetings()
+    }
+}
+
+@Composable
+fun OnboardingScreen(onContinueClicked: () -> Unit) {
+    Surface {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text("Welcome to the Basics Codelab!")
+            Button(
+                modifier = Modifier.padding(vertical = 24.dp),
+
+                // クリックしたらコールバック関数を実行する
+                onClick = onContinueClicked
+            ) {
+                Text("Continue")
+            }
+        }
+    }
+}
+
+@Composable
+private fun Greetings(names: List<String> = listOf("World", "Compose")) {
+    Column(modifier = Modifier.padding(vertical = 4.dp)) {
+        for (name in names) {
+            Greeting(name = name)
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview(){
+    BasicCodeLabTheme {
+        // onContinueClickedに空のラムダ式を渡すことで、なにもしないという挙動になる
+        OnboardingScreen(onContinueClicked = {})
+    }
+}
+
+
+@Composable
+private fun Greeting(name: String) {
     // 状態保存のために remember を使って値を保持する
     // 内部状態 expanded はプライベート変数とみなすこともできる
     val expanded = remember { mutableStateOf(false) }
@@ -63,14 +116,5 @@ fun Greeting(name: String) {
 fun DefaultPreview() {
     BasicCodeLabTheme {
         MyApp()
-    }
-}
-
-@Composable
-private fun MyApp(names: List<String> = listOf("World", "Compose")) {
-    Column(modifier = Modifier.padding(vertical = 4.dp)) {
-        for (name in names) {
-            Greeting(name = name)
-        }
     }
 }
